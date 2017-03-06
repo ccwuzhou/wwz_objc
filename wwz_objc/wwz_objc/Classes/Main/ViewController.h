1de9f61a9a5ff767e4ddf2efe7c7566008fc4b55
wwz2016 <wwz2016@163.com>
2017-03-06 16:19:29 +0800

    init

---
+++ wwz_objc/wwz_objc/Classes/Main/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wwz_objc
+//
+//  Created by wwz on 17/3/6.
+//  Copyright © 2017年 tijio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ wwz_objc/wwz_objc/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wwz_objc
+//
+//  Created by wwz on 17/3/6.
+//  Copyright © 2017年 tijio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
